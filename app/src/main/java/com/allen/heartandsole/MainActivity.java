fe3694ef1ecf333c59769a43f00e1837a6c3f288
allenw238 <allenwu750@gmail.com>
2021-05-19 15:34:35 -0700

    front page revamp start

--- app/src/main/java/com/allen/heartandsole/MainActivity.java
+++ app/src/main/java/com/allen/heartandsole/MainActivity.java
@@ -3,13 +3,14 @@ package com.allen.heartandsole;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setCustomView(R.layout.toolbar);
+        // Fixes the ImageView background height.
+        View bg = findViewById(R.id.unique_route_feature_bg),
+                darkener = findViewById(R.id.darkener);
+        bg.post(() -> {
+            ViewGroup.LayoutParams params = bg.getLayoutParams();
+            params.height = findViewById(R.id.unique_route_feature).getHeight();
+            bg.setLayoutParams(params);
+        });
+        darkener.post(() -> {
+            ViewGroup.LayoutParams params = darkener.getLayoutParams();
+            params.height = findViewById(R.id.unique_route_feature).getHeight();
+            darkener.setLayoutParams(params);
+            darkener.getBackground().setAlpha(100);
+        });
     }
 
     public void goToSignUp(View view) {
