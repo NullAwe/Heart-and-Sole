636cd1afd2341f4cad4b7b9baa6431d0b0907255
allenw238 <allenwu750@gmail.com>
2021-05-19 17:18:26 -0700

    commit before redo

--- app/src/main/java/com/allen/heartandsole/MainActivity.java
+++ app/src/main/java/com/allen/heartandsole/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper appModesDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,39 @@ public class MainActivity extends AppCompatActivity {
             darkener.setLayoutParams(params);
             darkener.getBackground().setAlpha(100);
         });
+        appModesDesc = findViewById(R.id.app_modes_description);
+        appModesDesc.setFlipInterval(7500);
+        appModesDesc.setInAnimation(this, R.anim.slide_in_right);
+        appModesDesc.setOutAnimation(this, R.anim.slide_out_left);
+//        appModesDesc.setInAnimation(this, R.anim.slide_in_left);
+//        appModesDesc.setOutAnimation(this, R.anim.slide_out_right);
+        appModesDesc.startFlipping();
+    }
+
+    public void moveVFLeft(View view) {
+        appModesDesc.setInAnimation(this, R.anim.slide_in_left);
+        appModesDesc.setOutAnimation(this, R.anim.slide_out_right);
+        appModesDesc.showPrevious();
+        resetFlip();
+    }
+
+    public void moveVFRight(View view) {
+        appModesDesc.setInAnimation(this, R.anim.slide_in_right);
+        appModesDesc.setOutAnimation(this, R.anim.slide_out_left);
+        appModesDesc.showNext();
+        resetFlip();
+    }
+
+    public void resetFlip() {
+        appModesDesc.stopFlipping();
+        appModesDesc.startFlipping();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                appModesDesc.setInAnimation(MainActivity.this, R.anim.slide_in_right);
+                appModesDesc.setOutAnimation(MainActivity.this, R.anim.slide_out_left);
+            }
+        }, 100);
     }
 
     public void goToSignUp(View view) {
